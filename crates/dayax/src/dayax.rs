71b75ab68538994822ada8dee7f5042884468375
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-01-14 19:22:31 +0100

    ref: improved route method

--- crates/dayax/src/dayax.rs
+++ crates/dayax/src/dayax.rs
@@ -1,4 +1,5 @@
 use axum::extract::State;
+use axum::routing::MethodFilter;
 use mlua::Lua;
 use mlua::UserData;
 use std::sync::Arc;
@@ -28,62 +29,53 @@ impl From<Dayax> for Router<Arc<Mutex<Lua>>> {
 }
 
 macro_rules! gen_dayax_http_verb {
-    ( $method_name:tt , $verb:tt ) => {
-        fn $method_name<'lua, 'this>(
+    ( $name:tt , $filter:expr ) => {
+        fn $name<'lua, 'this>(
             lua: &'lua Lua,
             this: &'this mut Self,
             (path, callback): (String, Function),
         ) -> mlua::Result<()> {
-            Dayax::route(lua, this, ($verb.into(), path, callback))
+            this.route(lua, &path, $filter, callback)
         }
     };
 }
 
 impl Dayax {
-    fn route<'lua, 'this>(
+    fn route<'lua>(
+        &mut self,
         lua: &'lua Lua,
-        this: &'this mut Self,
-        (method, path, callback): (String, String, Function),
+        path: &str,
+        method: MethodFilter,
+        callback: Function<'lua>,
     ) -> mlua::Result<()> {
         let info = callback.info();
         let line = info.line_defined;
-        let method = method.to_uppercase();
 
         let mut temp = Router::new();
-        std::mem::swap(&mut temp, &mut this.router);
+        std::mem::swap(&mut temp, &mut self.router);
         let registry_key = lua.create_registry_value(callback)?;
         let registry_key = Arc::new(registry_key);
         let handler = move |State(lua_mutex): State<Arc<_>>, req: DayaxRequest| async move {
             crate::handler::request_handler(&lua_mutex, registry_key.clone(), req).await
         };
-        let method_router = match method.as_str() {
-            "GET" => axum::routing::get(handler),
-            "POST" => axum::routing::post(handler),
-            "PUT" => axum::routing::put(handler),
-            "PATCH" => axum::routing::patch(handler),
-            "DELETE" => axum::routing::delete(handler),
-            "HEAD" => axum::routing::head(handler),
-            _ => axum::routing::any(handler),
-        };
-        this.router = temp.route(&path, method_router);
+        self.router = temp.route(&path, axum::routing::on(method, handler));
 
-        debug!(method, path, line, "Loaded request handler");
+        debug!(?method, path, line, "Loaded request handler");
 
         Ok(())
     }
 
-    gen_dayax_http_verb!(get, "GET");
-    gen_dayax_http_verb!(head, "HEAD");
-    gen_dayax_http_verb!(post, "POST");
-    gen_dayax_http_verb!(put, "PUT");
-    gen_dayax_http_verb!(patch, "PATCH");
-    gen_dayax_http_verb!(delete, "DELETE");
-    gen_dayax_http_verb!(any, "*");
+    gen_dayax_http_verb!(get, MethodFilter::GET);
+    gen_dayax_http_verb!(head, MethodFilter::HEAD);
+    gen_dayax_http_verb!(post, MethodFilter::POST);
+    gen_dayax_http_verb!(put, MethodFilter::PUT);
+    gen_dayax_http_verb!(patch, MethodFilter::PATCH);
+    gen_dayax_http_verb!(delete, MethodFilter::DELETE);
+    gen_dayax_http_verb!(any, MethodFilter::all());
 }
 
 impl UserData for Dayax {
     fn add_methods<'lua, M: mlua::UserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method_mut("route", Dayax::route);
         methods.add_method_mut("get", Dayax::get);
         methods.add_method_mut("head", Dayax::head);
         methods.add_method_mut("post", Dayax::post);
