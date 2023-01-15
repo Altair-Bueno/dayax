e64ea05771994fe3e72fbe7b63d1dc6d74472cc2
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-01-15 18:45:14 +0100

    new: added response tests

--- crates/dayax/tests/response.rs
+++ crates/dayax/tests/response.rs
@@ -35,20 +35,97 @@ fn extract_router(lua: &Lua) -> mlua::Result<DayaxRouter> {
 }
 
 #[rstest]
+#[case::empty(
+    r#"
+    dayax:get("/", function() 
+    end)"#,
+    StatusCode::OK,
+    b"",
+    vec![]
+)]
+#[case::nil(
+    r#"
+    dayax:get("/", function() 
+    end)"#,
+    StatusCode::OK,
+    b"",
+    vec![]
+)]
+#[case::string(
+    r#"
+    dayax:get("/", function()
+        return "Hello world"
+    end)"#,
+    StatusCode::OK,
+    b"Hello world",
+    vec![("Content-Type", "text/plain; charset=utf-8")]
+)]
+#[case::table_body_string(
+    r#"
+    dayax:get("/", function()
+        return { body = "hello world" }
+    end)"#,
+    StatusCode::OK,
+    b"hello world",
+    vec![("Content-Type", "text/plain; charset=utf-8")]
+)]
+#[case::table_body_json_array(
+    r#"
+    dayax:get("/", function()
+        return { body = { 1,2,3 } }
+    end)"#,
+    StatusCode::OK,
+    br#"[1,2,3]"#,
+    vec![("Content-Type", "application/json")]
+)]
+#[case::table_body_json_object(
+    r#"
+    dayax:get("/", function()
+        return { body = { hello = "world" } }
+    end)"#,
+    StatusCode::OK,
+    br#"{"hello":"world"}"#,
+    vec![("Content-Type", "application/json")]
+)]
+#[case::table_statuscode(
+    r#"
+    dayax:get("/", function()
+        return { statusCode = 418 }
+    end)"#,
+    StatusCode::IM_A_TEAPOT,
+    b"",
+    vec![]
+)]
+#[case::table_redirect(
+    r#"
+    dayax:get("/", function()
+        return { redirect = '/redirect' }
+    end)"#,
+    StatusCode::TEMPORARY_REDIRECT,
+    b"",
+    vec![("location", "/redirect")]
+)]
+#[case::error(
+    r#"
+    dayax:get("/", function()
+        error("My error")
+    end)"#,
+    StatusCode::INTERNAL_SERVER_ERROR,
+    b"Internal Server Error",
+    vec![]
+)]
 #[trace]
 #[tokio::test]
-async fn handler_without_return_responds_with_default_response(
+async fn handler_responds_with_expected_response(
     state: DayaxState,
+    #[case] lua_code: &str,
+    #[case] expected_status_code: StatusCode,
+    #[case] expected_body: &[u8],
+    #[case] expected_headers: Vec<(&str, &str)>,
 ) -> Result<(), Box<dyn Error>> {
     let router = {
         let lua = state.lock().await;
-        lua.load(
-            r#"
-        dayax:get("/", function() 
-        end)
-        "#,
-        )
-        .exec()?;
+        lua.load(lua_code).exec()?;
         extract_router(&lua)?
     };
 
@@ -57,7 +134,16 @@ async fn handler_without_return_responds_with_default_response(
         .oneshot(Request::builder().uri("/").body(Body::empty())?)
         .await?;
 
-    assert_that!(response.status()).is_equal_to(StatusCode::OK);
+    assert_that!(response.status()).is_equal_to(expected_status_code);
+
+    let headers = response.headers();
+    for (expected_name, expected_value) in expected_headers {
+        let value = headers.get(expected_name).expect("Missing header");
+        assert_that!(value.to_str()?).is_equal_to(expected_value);
+    }
+
+    let body = hyper::body::to_bytes(response.into_body()).await?;
+    assert_that(&&body[..]).is_equal_to(expected_body);
 
     Ok(())
 }
